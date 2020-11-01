366e36d4e5a63414ffbf0cb4faca6dc20aa31114
LIN <stefanzhang9@outlook.com>
2020-11-01 21:58:43 +0100

    Further development on the plot tool

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# VideoPose3D-for-multi-person-tracking
+## VideoPose3D-for-multi-person-tracking
 This project aims at adjusting the VideoPose3D project from Dario Pavllo, in order to track the trajectories of multiple people and predict the keypoints of their skeletons with respective to their root points.
+
+## How to use it?
+
+# 1. Git clone the VideoPose3D project:
+	https://github.com/facebookresearch/VideoPose3D
+# 2. After we finish Step 4: creating a custom dataset mentioned in INFERENCE.md of VideoPose3D project, we replace run.py and relative files (arguments.py and visualization.py in 'common' folder)
+
+
+
--- plot_3d_points_Patrick.py
+++ plot_3d_points_Patrick.py
@@ -1,5 +1,7 @@
 """
 Plot animation of 3d VideoPose3D output npy file.
+Auther: Patrick and Lin
+
 """
 
 import matplotlib.pyplot as plt
@@ -14,26 +16,72 @@ FILE = "output_3d_after_camera_to_world.npy"
 
 data = np.load(FILE)
 
+# pose color
+keypoint_num = 17
+joints_right_2d = [2, 4, 6, 8, 10, 12, 14, 16]
+colors_2d = np.full(keypoint_num, 'black')
+colors_2d[joints_right_2d] = 'red'
+mul_colors_2d = np.expand_dims(colors_2d, 0).repeat(data.shape[0], axis=0)
+colors_2d = mul_colors_2d.reshape(data.shape[0] * data.shape[2])
+parents = [-1, 0, 1, 2, 0, 4, 5, 0, 7, 8, 9, 8, 11, 12, 8, 14, 15]
+skeleton_joints_right = [1, 2, 3, 14, 15, 16]
+
+initialized = False
+
+lines_3d = []
+lines_3d.append([])
 artists = []
 
+
+
+
+
+
 def plot_at_frame(fr):
-    global artist, ax
+    # nonlocal initialized
+    global artist, ax, lines_3d
     x, y, z = data[:, fr, :, :].T
     if len(artists) == 0:
         for i in range(len(x)):
             artists.append(ax.scatter(x[i], y[i], z[i], label=str(i)))
+
+        for mul in range(x.shape[1]):
+            for j, j_parent in enumerate(parents):
+                if j_parent == -1:
+                    continue
+
+                col = 'red' if j in skeleton_joints_right else 'black'
+
+                lines_3d[0].append(ax.plot([x[j][mul], x[j_parent][mul]],
+                                           [y[j][mul], y[j_parent][mul]],
+                                           [z[j][mul], z[j_parent][mul]], zdir='z', c=col))
         ax.legend()
     else:
+        # keypoint
         for i, artist in enumerate(artists):
             artist._offsets3d = (x[i], y[i], z[i])
+
+        # pose
+        for mul in range(x.shape[1]):
+            for j, j_parent in enumerate(parents):
+                if j_parent == -1:
+                    continue
+
+                lines_3d[0][mul * 16 + j - 1][0].set_xdata(np.array([x[j][mul], x[j_parent][mul]]))
+                lines_3d[0][mul * 16 + j - 1][0].set_ydata(np.array([y[j][mul], y[j_parent][mul]]))
+                lines_3d[0][mul * 16 + j - 1][0].set_3d_properties(np.array([z[j][mul], z[j_parent][mul]]),zdir='z')
+
     return artists
 
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.set_xlim([-1, 1])
-ax.set_ylim([-1, 1])
-ax.set_zlim([-1, 1])
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+# ax.set_xlim([-1, 1])
+# ax.set_ylim([-1, 1])
+# ax.set_zlim([-1, 1])
 
 a = animation.FuncAnimation(fig, plot_at_frame, frames=range(data.shape[1]), repeat=True)
 plt.show()
